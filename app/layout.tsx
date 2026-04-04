86e450c08a9ce26db3eea96d1b3e2ec9a14a39f6
Riddhima04022006 <riddhima2903@gmail.com>
2026-04-04 17:35:47 +0530

    Edited layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,26 @@ import { Mulish } from 'next/font/google';
 import { Bebas_Neue, DM_Mono } from 'next/font/google';
 import CustomCursor from '@/components/CustomCursor';
 import NavigationWrapper from '@/components/NavigationWrapper';
-import localFont from 'next/font/local'
-
+import localFont from 'next/font/local';
+import type { Viewport } from 'next';
 
 import './globals.css';
 
 export const mulish = Mulish({
   subsets: ['latin'],
-  display: 'swap', 
-  variable: '--font-mulish', 
+  display: 'swap',
+  variable: '--font-mulish',
 });
 
-
-export const syne = Syne({ 
+export const syne = Syne({
   subsets: ['latin'],
-  weight: ['400', '700', '800'], 
+  weight: ['400', '700', '800'],
   display: 'swap',
 });
 
 export const orbitronio = localFont({
   src: './fonts/Orbitronio.ttf',
-  variable: '--font-orbitronio', // This creates a CSS variable we can use in Tailwind
+  variable: '--font-orbitronio',
   display: 'swap',
 });
 
@@ -33,15 +32,23 @@ export const infiniteBeyond = localFont({
   display: 'swap',
 });
 
-
 export const bebas = Bebas_Neue({ weight: '400', subsets: ['latin'], variable: '--font-bebas' });
 export const dmMono = DM_Mono({ weight: '400', subsets: ['latin'], variable: '--font-dm-mono' });
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{cursor: "none"}} className={` bg-black text-white ${bebas.variable} ${dmMono.variable} ${infiniteBeyond.variable} cursor-none`}>
+      <body
+        style={{ cursor: 'none' }}
+        className={`bg-black text-white ${bebas.variable} ${dmMono.variable} ${infiniteBeyond.variable} cursor-none`}
+      >
         <CustomCursor />
         <NavigationWrapper />
         {children}
