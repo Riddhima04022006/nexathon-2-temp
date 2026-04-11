c864437d7c1434b7e021ae5df3eb37c29f769ceb
Riddhima04022006 <riddhima2903@gmail.com>
2026-04-12 02:27:43 +0530

    Fixed sitemap2

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.nexathonv2.in/",
+    },
+    {
+      url: "https://www.nexathonv2.in/about",
+    },
+    {
+      url: "https://www.nexathonv2.in/contact",
+    },
+  ];
+}
